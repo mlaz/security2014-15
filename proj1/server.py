6837c1168f1d9911b5050cbf90acb57fef58d720
Miguel <miguellazev@gmail.com>
2014-11-04 21:34:47 +0000

    server.py: Code structure improved, basic error handling and comments added.

--- proj1/server.py
+++ proj1/server.py
@@ -8,93 +8,332 @@ from twisted.enterprise import adbapi
 #from twisted.web.server import NOT_DONE_YET
 
 from datetime import datetime
-import simplejson
+import json
 
 # dbfilename = "test.sqlite"
 # dbpool = adbapi.ConnectionPool("sqlite3", dbfilename, check_same_thread=False)
 
-class Session(Resource):
-    isLeaf = True
+# class Session(Resource):
+#     isLeaf = True
 
-    def render_POST(self, request):
-        pprint(request.__dict__)
-        #START SESSION
-        #END SESSION
-        newdata = request.content.getvalue()
-        print newdata
-        return "hello form"
+#     def render_POST(self, request):
+#         pprint(request.__dict__)
+#         #START SESSION
+#         #END SESSION
+#         newdata = request.content.getvalue()
+#         print newdata
+#         return "hello form"
 
+# The PBoxes resource
 class PBoxes(Resource):
     isLeaf = True
 
+    # GET Methods:
+    #
+    # list: To list all pboxes.
+    # 'method' = "list"
+    #
+    # get_mdata: To a pbox's metadata.
+    # 'method' = "get_mdata"
+    # 'fileid' = <the file id>
     def render_GET(self, request):
-        #GET THE LIST OF ALL PBOXES
-        #GET METADATA FROM A SINGLE PBOX
+        error = None;
+        if 'method' not in request.args.keys():
+            return json.dumps(
+                {"error": "Invalid Request", "reason": "Argument 'method' not specified."},
+                sort_keys=True, encoding="utf-8")
+
+        # list:
+        if request.args['method'] == ['list']:
+            print request.args['method']
+        # get_mdata:
+        elif request.args['method'] == ['get_mdata']:
+            print request.args['method']
+
+        else:
+            error = {"error": "Invalid Request", "reason": "Unknown method for this resource."}
+
+        if error != None:
+            print request.args['method']
+            pprint(request.__dict__)
+            return json.dumps(error, sort_keys=True, encoding="utf-8")
+
         return ''
 
-    def render_POST(self, request):
+
+    # PUT Methods:
+    #
+    # register: To register (create) a new PBox.
+    # 'method' = "register"
+    # 'ccnumber' = <user's Portuguese Cityzen Card number>
+    # 'name' = <user's reak name>
+    def render_PUT(self, request):
+        error = None;
+        if 'method' not in request.args.keys():
+            return json.dumps(
+                {"error": "Invalid Request", "reason": "Argument 'method' not specified."},
+                sort_keys=True, encoding="utf-8")
+
         pprint(request.__dict__)
-        #REGISTER PBOX
+
+        # register:
+        if request.args['method'] == ['register']:
+            print request.args['method']
+
+        else:
+            error = {"error": "Invalid Request", "reason": "Unknown method for this resource."}
+
+        if error != None:
+            return json.dumps(error, sort_keys=True, encoding="utf-8")
+
         newdata = request.content.getvalue()
         print newdata
         return "hello form"
 
+# The Files resource
 class Files(Resource):
     isLeaf = True
 
+    # GET Methods:
+    #
+    # list: To list all files on user's pbox.
+    # 'method' = "getshared"
+    # 'fileid' = <the file id>
+    #
+    # getfile: To download a file from user's pbox.
+    # 'method' = "getfile"
+    # 'fileid' = <the file id>
     def render_GET(self, request):
-        #LIST ALL FILES ON USER'S PBOX
-        #DOWNLOAD A FILE
+        error = None;
+        if 'method' not in request.args.keys():
+            return json.dumps(
+                {"error": "Invalid Request", "reason": "Argument 'method' not specified."},
+                sort_keys=True, encoding="utf-8")
+
+        # list:
+        if request.args['method'] == ['list']:
+            print request.args['method']
+
+        # getfile:
+        elif request.args['method'] == ['getfile']:
+            print request.args['method']
+
+        else:
+            error = {"error": "Invalid Request", "reason": "Unknown method for this resource."}
+
+        if error != None:
+            return json.dumps(error, sort_keys=True, encoding="utf-8")
+
+
         return ''
 
+    # POST Methods:
+    #
+    # updatefile: To update (reupload) a file.
+    # 'method' = "updatefile"
+    # 'fileid' = <the file id>
     def render_POST(self, request):
+        error = None;
+        if 'method' not in request.args.keys():
+            return json.dumps(
+                {"error": "Invalid Request", "reason": "Argument 'method' not specified."},
+                sort_keys=True, encoding="utf-8")
+
         pprint(request.__dict__)
-        #UPLOAD A FILE
+
+        # updatefile:
+        if request.args['method'] == ['updatefile']:
+            print request.args['method']
+
+        else:
+            error = {"error": "Invalid Request", "reason": "Unknown method for this resource."}
+
+        if error != None:
+            return json.dumps(error, sort_keys=True, encoding="utf-8")
+
+
         newdata = request.content.getvalue()
         print newdata
         return "hello user"
 
+    # PUT Methods:
+    #
+    # putfile: To register (create) a new PBox.
+    # 'method' = "putfile"
+    # 'name' = <file name>
     def render_PUT(self, request):
+        error = None;
+        if 'method' not in request.args.keys():
+            return json.dumps(
+                {"error": "Invalid Request", "reason": "Argument 'method' not specified."},
+                sort_keys=True, encoding="utf-8")
+
         pprint(request.__dict__)
-        #UPDATE A FILE (AKA CHANGE THE FILE)
+
+        # putfile:
+        if request.args['method'] == ['putfile']:
+            print request.args['method']
+
+        else:
+            error = {"error": "Invalid Request", "reason": "Unknown method for this resource."}
+
+        if error != None:
+            return json.dumps(error, sort_keys=True, encoding="utf-8")
+
+
         newdata = request.content.getvalue()
         print newdata
         return "hello user"
 
+    # DELETE Methods:
+    #
+    # delete: To delete a file.
+    # 'method' = "delete"
+    # 'fileid' = <the file id>
     def render_DELETE(self, request):
+        error = None;
+        if 'method' not in request.args.keys():
+            return json.dumps(
+                {"error": "Invalid Request", "reason": "Argument 'method' not specified."},
+                sort_keys=True, encoding="utf-8")
+
         pprint(request.__dict__)
-        #DELETE A FILE
+
+        # delete:
+        if request.args['method'] == ['delete']:
+            print request.args['method']
+
+        else:
+            error = {"error": "Invalid Request", "reason": "Unknown method for this resource."}
+
+        if error != None:
+            return json.dumps(error, sort_keys=True, encoding="utf-8")
+
+
         newdata = request.content.getvalue()
         print newdata
         return "hello user"
 
-
+# The Shares resource
 class Shares(Resource):
     isLeaf = True
 
+    # GET Methods:
+    #
+    # getshared: To download a shared file.
+    # 'method' = "getshared"
+    # 'fileid' = <the file id>
     def render_GET(self, request):
+        error = None;
+        if 'method' not in request.args.keys():
+            return json.dumps(
+                {"error": "Invalid Request", "reason": "Argument 'method' not specified."},
+                sort_keys=True, encoding="utf-8")
         pprint(request.__dict__)
-        #GET A SHARE (AKA DOWNLOAD THE SHARED FILE)
+
+        # getshared:
+        if request.args['method'] == ['getshared']:
+            print request.args['method']
+
+        else:
+            error = {"error": "Invalid Request", "reason": "Unknown method for this resource."}
+
+        if error != None:
+            return json.dumps(error, sort_keys=True, encoding="utf-8")
+
         return "hello user"
 
+    # POST Methods:
+    #
+    # sharefile: To share a file.
+    # 'method' = "sharefile"
+    # 'fileid' = <the file id>
+    # 'ruserid' = <recipient's user id> TODO: define what this is!
     def render_POST(self, request):
+        error = None;
+        if 'method' not in request.args.keys():
+            return json.dumps(
+                {"error": "Invalid Request", "reason": "Argument 'method' not specified."},
+                sort_keys=True, encoding="utf-8")
+
         pprint(request.__dict__)
-        #SHARE A FILE (AKA CREATE A NEW SHARE)
+
+        # sharefile:
+        if request.args['method'] == ['sharefile']:
+            print request.args['method']
+
+        else:
+            error = {"error": "Invalid Request", "reason": "Unknown method for this resource."}
+
+        if error != None:
+            return json.dumps(error, sort_keys=True, encoding="utf-8")
+
         newdata = request.content.getvalue()
         print newdata
         return "hello user"
 
+    # PUT Methods:
+    #
+    # updateshare: To update a share (ex.: change write permissions).
+    # 'method' = "updateshare"
+    # 'fileid' = <the file id>
+    # 'ruserid' = <recipient's user id> TODO: define what this is!
+    # 'attrname' = <the share's attribute to change>
+    # 'newval' = <the new value>
+    #
+    # updatesfile: To update (modify) a shared file.
+    # 'method' = "updatesfile"
+    # 'fileid' = <the file id>
     def render_PUT(self, request):
+        error = None;
+        if 'method' not in request.args.keys():
+            return json.dumps(
+                {"error": "Invalid Request", "reason": "Argument 'method' not specified."},
+                sort_keys=True, encoding="utf-8")
+
         pprint(request.__dict__)
-        #UPDATE A SHARE (I.E. CHANGE DE WRITE PERMISSIONS)
-        #UPDATE A SHARED FILE (AKA CHANGE THE FILE)
+
+        # updateshare:
+        if request.args['method'] == ['updateshare']:
+            print request.args['method']
+
+        # updatesfile:
+        elif request.args['method'] == ['updatesfile']:
+            print request.args['method']
+
+        else:
+            error = {"error": "Invalid Request", "reason": "Unknown method for this resource."}
+
+        if error != None:
+            return json.dumps(error, sort_keys=True, encoding="utf-8")
+
         newdata = request.content.getvalue()
         print newdata
         return "hello user"
 
+    # DELETE Methods:
+    #
+    # delete: To delete (unshare) a shared file.
+    # 'method' = "delete"
+    # 'fileid' = <the file id>
     def render_DELETE(self, request):
+        error = None;
+
+        if 'method' not in request.args.keys():
+            error = {"error": "Invalid Request", "reason": "Argument 'method' not specified."}
+            return json.dumps(error, sort_keys=True, encoding="utf-8")
+
         pprint(request.__dict__)
-        #DELETE A SHARE (AKA UNSHARE)
+
+        # delete:
+        if request.args['method'] == ['delete']:
+            print request.args['method']
+        else:
+            error = {"error": "Invalid Request", "reason": "Unknown method for this resource."}
+
+        if error != None:
+            return json.dumps(error, sort_keys=True, encoding="utf-8")
+
         newdata = request.content.getvalue()
         print newdata
         return "hello user"
@@ -102,7 +341,7 @@ class Shares(Resource):
 
 if __name__ == "__main__":
     root = Resource()
-    root.putChild("session", Session())
+#    root.putChild("session", Session())
     root.putChild("pboxes", PBoxes())
     root.putChild("files", Files())
     root.putChild("shares", Shares())
