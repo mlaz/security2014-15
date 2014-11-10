ebe8ae10296bdd762fc0bcf2a9b47c705182946a
jlrosa <jlrosa@ua.pt>
2014-11-10 03:20:23 +0000

    error handling; get ticket, get mdata, get key;

--- proj1/client/sfbx_client_utils.py
+++ proj1/client/sfbx_client_utils.py
@@ -60,7 +60,53 @@ def formatKey(response):
     if (response["status"] == ["error"]):
         print(response["error"])
     else:
-        print(response["list"])
+        print(response["key"])
+        
+class getTicket(Protocol):
+    def __init__(self, finished):
+        self.finished = finished
+        self.total_response = ""
+
+    def dataReceived(self, bytes):
+        self.total_response += bytes
+
+    def connectionLost(self, reason):
+        print 'The ticket is:\n', formatTicket(self.total_response)
+
+def formatTicket(response):
+    response = json.loads(response, object_hook=_decode_dict)
+    if (response["status"] == ["error"]):
+        print(response["error"])
+    else:
+        s = response["ticket"]
+        #s = s.strip("(")
+        #s = s.strip(")")
+        #s = s.strip(",")
+        #s = s.strip('"')
+        print s
+        
+class getMData(Protocol):
+    def __init__(self, finished):
+        self.finished = finished
+        self.total_response = ""
+
+    def dataReceived(self, bytes):
+        self.total_response += bytes
+
+    def connectionLost(self, reason):
+        print 'User Data:\n', formatMData(self.total_response)
+
+def formatMData(response):
+    response = json.loads(response, object_hook=_decode_dict)
+    if (response["status"] == ["error"]):
+        print(response["error"])
+    else:
+        s = response["data"]
+        #s = s.strip("(")
+        #s = s.strip(")")
+        #s = s.strip(",")
+        #s = s.strip('"')
+        print s
 
 def formatResponse(response):
     response = json.loads(response, object_hook=_decode_dict)
@@ -119,25 +165,29 @@ class SafeBoxClient():
 
         agent = Agent(reactor)
         s = line.split()
-        if s[1].lower() == "pboxes":
-            d = agent.request(
-                    'GET',
-                    'http://localhost:8000/pboxes/?method=list',
-                    Headers({'User-Agent': ['Twisted Web Client Example'],
-                    'Content-Type': ['text/x-greeting']}),
-                    None)
+        if len(s) != 2:
+            print "Error: invalid arguments!"
+            return
+        else:
+            if s[1].lower() == "pboxes":
+                d = agent.request(
+                        'GET',
+                        'http://localhost:8000/pboxes/?method=list',
+                        Headers({'User-Agent': ['Twisted Web Client Example'],
+                        'Content-Type': ['text/x-greeting']}),
+                        None)
 
-        elif s[1].lower() == "files":
-            d = agent.request(
-                    'GET',
-                    'http://localhost:8000/files/?method=list&pboxid=1',
-                    Headers({'User-Agent': ['Twisted Web Client Example'],
-                    'Content-Type': ['text/x-greeting']}),
-                    None)
+            elif s[1].lower() == "files":
+                d = agent.request(
+                        'GET',
+                        'http://localhost:8000/files/?method=list&pboxid=1',
+                        Headers({'User-Agent': ['Twisted Web Client Example'],
+                        'Content-Type': ['text/x-greeting']}),
+                        None)
 
-        d.addCallback(handleList_cb)
+            d.addCallback(handleList_cb)
 
-        return NOT_DONE_YET
+            return NOT_DONE_YET
 
     def handleGet(self, line):
         def handleGet_cb(response, file):
@@ -154,19 +204,23 @@ class SafeBoxClient():
 
         agent = Agent(reactor)
         s = line.split()
-        if s[1].lower() == "file":
-            fileId = s[2].lower()
-            file = open(fileId, "w")
-            d = agent.request(
-                    'GET',
-                    'http://localhost:8000/files/?method=getfile&fileid='+ fileId +'&pboxid=1',
-                    Headers({'User-Agent': ['Twisted Web Client Example'],
-                    'Content-Type': ['text/x-greeting']}),
-                    None)
+        if len(s) != 3:
+            print "Error: invalid arguments!"
+            return
+        else:
+            if s[1].lower() == "file":
+                fileId = s[2].lower()
+                file = open(fileId, "w")
+                d = agent.request(
+                        'GET',
+                        'http://localhost:8000/files/?method=getfile&fileid='+ fileId +'&pboxid=1',
+                        Headers({'User-Agent': ['Twisted Web Client Example'],
+                        'Content-Type': ['text/x-greeting']}),
+                        None)
 
-        d.addCallback(handleGet_cb, file)
+            d.addCallback(handleGet_cb, file)
 
-        return NOT_DONE_YET
+            return NOT_DONE_YET
 
     def handlePut(self, line):
         return
@@ -183,36 +237,46 @@ class SafeBoxClient():
         
         agent = Agent(reactor)
         s = line.split()
-        username = s[1]
-        ccnumber = s[2]
-        password = s[3]
+        if len(s) != 4:
+            print "Error: invalid arguments"
+            return
+        else:
+            username = s[1]
+            ccnumber = s[2]
+            password = s[3]
 
-        #if not os.path.exists(username):
-        #    os.makedirs(username)
-        #fn = username + "/private.pem"
-        #if not os.path.exists(fn):
-        #    open(fn, 'w').close()
-        #fn = username + "/public.pem"
-        #if not os.path.exists(fn):
-        #    open(fn, 'w').close()
+            #if not os.path.exists(username):
+            #    os.makedirs(username)
+            #fn = username + "/private.pem"
+            #if not os.path.exists(fn):
+            #    open(fn, 'w').close()
+            #fn = username + "/public.pem"
+            #if not os.path.exists(fn):
+            #    open(fn, 'w').close()
 
-        ci = ClientIdentity("rsakeys", password)
+            ci = ClientIdentity("rsakeys", password)
 
-        body = FileBodyProducer(StringIO(ci.pub_key.exportKey('PEM')))
-        d = agent.request(
-                    'PUT',
-                    'http://localhost:8000/pboxes/?method=register&ccid='+ ccnumber +'&name=' + username,
-                    Headers({'User-Agent': ['Twisted Web Client Example'],
-                    'Content-Type': ['text/x-greeting']}),
-                    body)
+            body = FileBodyProducer(StringIO(ci.pub_key.exportKey('PEM')))
+            d = agent.request(
+                        'PUT',
+                        'http://localhost:8000/pboxes/?method=register&ccid='+ ccnumber +'&name=' + username,
+                        Headers({'User-Agent': ['Twisted Web Client Example'],
+                        'Content-Type': ['text/x-greeting']}),
+                        body)
 
-        d.addCallback(handleRegister_cb)
-        return NOT_DONE_YET
+            d.addCallback(handleRegister_cb)
+            return NOT_DONE_YET
     
     def handleLogin(self, line):
         s = line.split()
-        username = s[1]
-        password = s[2]
+        if len(s) != 3:
+            print "Error: invalid arguments"
+            return
+        else:
+            username = s[1]
+            password = s[2]
+
+            return "sucess"
 
         #TODO        
 
@@ -236,5 +300,51 @@ class SafeBoxClient():
                     None)
 
         d.addCallback(handleGetKey_cb)
+
+        return NOT_DONE_YET
+
+    def handleGetTicket(self):
+        def handleGetTicket_cb(response):
+            print 'Response version: ', response.version
+            print 'Response code: ', response.code
+            print 'Response phrase: ', response.phrase
+            print 'Response headers: '
+            print pformat(list(response.headers.getAllRawHeaders()))
+            defer = Deferred()
+            response.deliverBody(getTicket(defer))
+            return NOT_DONE_YET
+
+        agent = Agent(reactor)
+        d = agent.request(
+                'GET',
+                'http://localhost:8000/session/?method=getticket&ccid=123456789',
+                Headers({'User-Agent': ['Twisted Web Client Example'],
+                'Content-Type': ['text/x-greeting']}),
+                None)
+
+        d.addCallback(handleGetTicket_cb)
+
+        return NOT_DONE_YET
+
+    def handleGetMData(self):
+        def handleGetMData_cb(response):
+            print 'Response version: ', response.version
+            print 'Response code: ', response.code
+            print 'Response phrase: ', response.phrase
+            print 'Response headers: '
+            print pformat(list(response.headers.getAllRawHeaders()))
+            defer = Deferred()
+            response.deliverBody(getMData(defer))
+            return NOT_DONE_YET
+
+        agent = Agent(reactor)
+        d = agent.request(
+			'GET',
+            'http://localhost:8000/pboxes/?method=get_mdata&ccid=123456789',
+            Headers({'User-Agent': ['Twisted Web Client Example'],
+            'Content-Type': ['text/x-greeting']}),
+            None)
+
+        d.addCallback(handleGetMData_cb)
 
         return NOT_DONE_YET
