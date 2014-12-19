afd6b72849706d82c78c4a3dc999760ec9ce94ef
security <security@security.(none)>
2014-12-19 22:39:59 +0000

    functions for get a list of all the certificates in the CC

--- proj2/client/cc_utils.py
+++ proj2/client/cc_utils.py
@@ -206,3 +206,147 @@ def getPubCert(pin=None, lib=None):
 
 		except PyKCS11.PyKCS11Error, e:
 			print "Error: ", e
+
+
+def getPubKey(pin=None, lib=None):
+	if lib == None:
+		lib = "/usr/local/lib/libpteidpkcs11.so"
+	if pin == None:
+		pin = 6214
+	pin_available = True
+
+	pkcs11 = PyKCS11.PyKCS11Lib()
+	pkcs11.load(lib)
+
+	slots = pkcs11.getSlotList()
+
+	for s in slots:
+		try:
+			session = pkcs11.openSession(s)
+			try:
+				session.login(pin)
+			except:
+				pass
+
+			objects = session.findObjects()
+
+			
+			all_attr = PyKCS11.CKA.keys()
+			#remover os atributos privados
+			all_attr.remove(PyKCS11.CKA_PRIVATE_EXPONENT)
+			all_attr.remove(PyKCS11.CKA_PRIME_1)
+			all_attr.remove(PyKCS11.CKA_PRIME_2)
+			all_attr.remove(PyKCS11.CKA_EXPONENT_1)
+			all_attr.remove(PyKCS11.CKA_EXPONENT_2)
+			all_attr.remove(PyKCS11.CKA_COEFFICIENT)
+			#usar apenas os valores int
+			all_attr = [e for e in all_attr if isinstance(e, int)]
+
+			for obj in objects:
+				attr = session.getAttributeValue(obj, all_attr)
+				attrDict = dict(zip(all_attr, attr))
+				i = 0
+				for q, a in zip(all_attr, attr):
+					if attrDict[PyKCS11.CKA_CLASS] == PyKCS11.CKO_PUBLIC_KEY \
+					   and attrDict[PyKCS11.CKA_KEY_TYPE] == PyKCS11.CKK_RSA:
+						m = attrDict[PyKCS11.CKA_MODULUS]
+						e = attrDict[PyKCS11.CKA_PUBLIC_EXPONENT]
+						return [dump(''.join(map(chr, e)), 16), dump(''.join(map(chr, m)), 16)]
+			
+			if pin_available:
+				try:
+					session.logout()
+				except:
+					pass
+
+			session.closeSession()
+
+		except PyKCS11.PyKCS11Error, e:
+			print "Error: ", e
+
+
+def getCertList(pin=None, lib=None):
+	if lib == None:
+		lib = "/usr/local/lib/libpteidpkcs11.so"
+	if pin == None:
+		pin = 6214
+	pin_available = True
+
+	pkcs11 = PyKCS11.PyKCS11Lib()
+	pkcs11.load(lib)
+
+	slots = pkcs11.getSlotList()
+
+	certList = []
+	labels = ["CITIZEN AUTHENTICATION CERTIFICATE", "AUTHENTICATION SUB CA", "CITIZEN SIGNATURE CERTIFICATE", "SIGNATURE SUB CA"]
+
+	for s in slots:
+		try:
+			session = pkcs11.openSession(s)
+			try:
+				session.login(pin)
+			except:
+				pass
+
+			objects = session.findObjects()
+
+			
+			all_attr = PyKCS11.CKA.keys()
+			#remover os atributos privados
+			all_attr.remove(PyKCS11.CKA_PRIVATE_EXPONENT)
+			all_attr.remove(PyKCS11.CKA_PRIME_1)
+			all_attr.remove(PyKCS11.CKA_PRIME_2)
+			all_attr.remove(PyKCS11.CKA_EXPONENT_1)
+			all_attr.remove(PyKCS11.CKA_EXPONENT_2)
+			all_attr.remove(PyKCS11.CKA_COEFFICIENT)
+			#usar apenas os valores int
+			all_attr = [e for e in all_attr if isinstance(e, int)]
+			
+			for obj in objects:
+				i = 0
+				attr = session.getAttributeValue(obj, all_attr)
+				attrDict = dict(zip(all_attr, attr))
+				
+				for q, a in zip(all_attr, attr):
+					if attrDict[PyKCS11.CKA_CLASS] == PyKCS11.CKO_CERTIFICATE:
+						if attrDict[PyKCS11.CKA_LABEL] == labels[0] and session.isBin(q):
+							i += 1
+							if a and i == 3:
+								certList.append(labels[0])
+								certList.append(dump(''.join(map(chr, a)), 16))
+								
+				for q, a in zip(all_attr, attr):
+					if attrDict[PyKCS11.CKA_CLASS] == PyKCS11.CKO_CERTIFICATE:
+						if attrDict[PyKCS11.CKA_LABEL] == labels[1] and session.isBin(q):
+							i += 1
+							if a and i == 3:
+								certList.append(labels[1])
+								certList.append(dump(''.join(map(chr, a)), 16))
+				
+				for q, a in zip(all_attr, attr):
+					if attrDict[PyKCS11.CKA_CLASS] == PyKCS11.CKO_CERTIFICATE:
+						if attrDict[PyKCS11.CKA_LABEL] == labels[2] and session.isBin(q):
+							i += 1
+							if a and i == 3:
+								certList.append(labels[2])
+								certList.append(dump(''.join(map(chr, a)), 16))
+								
+				for q, a in zip(all_attr, attr):
+					if attrDict[PyKCS11.CKA_CLASS] == PyKCS11.CKO_CERTIFICATE:
+						if attrDict[PyKCS11.CKA_LABEL] == labels[3] and session.isBin(q):
+							i += 1
+							if a and i == 3:
+								certList.append(labels[3])
+								certList.append(dump(''.join(map(chr, a)), 16))
+						
+			if pin_available:
+				try:
+					session.logout()
+				except:
+					pass
+
+			session.closeSession()
+			return certList
+
+		except PyKCS11.PyKCS11Error, e:
+			print "Error: ", e
